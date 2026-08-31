846bfce5384fd1707f6f33880c9a4655e2aa3f90
bozo.kopic <bozo@kopic.xyz>
2026-08-31 16:15:09 +0200

    test view fix

--- test_pytest/test_server/test_view.py
+++ test_pytest/test_server/test_view.py
@@ -1,3 +1,5 @@
+import pytest
+
 import hat.gui.server.view
 
 
@@ -9,8 +11,8 @@ async def test_empty_view_manager():
     view_name = manager.get_view({'nonexistent'})
     assert view_name is None
 
-    view_path = manager.get_view_path('nonexistent')
-    assert view_path is None
+    with pytest.raises(Exception):
+        manager.get_view_paths('nonexistent')
 
     await manager.async_close()
 
@@ -20,13 +22,13 @@ async def test_empty_view_manager():
 async def test_get_view(tmp_path):
     view_confs = [{'name': 'abc',
                    'roles': ['operator'],
-                   'view_path': str(tmp_path / 'abc.js')},
+                   'paths': [{'path': str(tmp_path / 'abc.js')}]},
                   {'name': 'def',
                    'roles': ['operator', 'admin'],
-                   'view_path': str(tmp_path / 'def.js')},
+                   'paths': [{'path': str(tmp_path / 'def.js')}]},
                   {'name': 'ghi',
                    'roles': ['guest'],
-                   'builtin': 'login'}]
+                   'paths': [{'builtin': 'login'}]}]
 
     manager = hat.gui.server.view.ViewManager(view_confs)
 
@@ -48,29 +50,29 @@ async def test_get_view(tmp_path):
     await manager.async_close()
 
 
-async def test_get_view_path(tmp_path):
+async def test_get_view_paths(tmp_path):
     view_confs = [{'name': 'abc',
                    'roles': ['operator'],
-                   'view_path': str(tmp_path / 'abc.js')},
+                   'paths': [{'path': str(tmp_path / 'abc.js')}]},
                   {'name': 'def',
                    'roles': ['operator', 'admin'],
-                   'view_path': str(tmp_path / 'def.js')},
+                   'paths': [{'path': str(tmp_path / 'def.js')}]},
                   {'name': 'ghi',
                    'roles': ['guest'],
-                   'builtin': 'login'}]
+                   'paths': [{'builtin': 'login'}]}]
 
     manager = hat.gui.server.view.ViewManager(view_confs)
 
-    view_path = manager.get_view_path('abc')
-    assert view_path == tmp_path / 'abc.js'
+    view_paths = manager.get_view_paths('abc')
+    assert list(view_paths) == [tmp_path / 'abc.js']
 
-    view_path = manager.get_view_path('def')
-    assert view_path == tmp_path / 'def.js'
+    view_paths = manager.get_view_paths('def')
+    assert list(view_paths) == [tmp_path / 'def.js']
 
-    view_path = manager.get_view_path('ghi')
-    assert view_path is not None
+    view_paths = manager.get_view_paths('ghi')
+    assert view_paths is not None
 
-    view_path = manager.get_view_path('nonexistent')
-    assert view_path is None
+    with pytest.raises(Exception):
+        manager.get_view_paths('nonexistent')
 
     await manager.async_close()
